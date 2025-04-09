64274be3585ad57de3fafbded48ba6a4552a2b65
pawan <spavandsp@gmail.com>
2025-04-10 00:46:30 +0530

    fix: added status to response

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,14 +1,16 @@
 import { NextFunction, Request, Response } from "express";
 import jwt, { Jwt } from "jsonwebtoken";
 
-const JWT_PASSWORD = process.env.JWT_PASSWORD || "password";
-
 export interface JwtPayload {
     userId: string;
     role: string;
 }
-  
-export const userMiddleware = (req: Request, res: Response, next: NextFunction) => {
+
+export const userMiddleware = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
     const header = req.header("authorization");
     // console.log(header);
     const token = header?.split(" ")[1];
@@ -21,13 +23,14 @@ export const userMiddleware = (req: Request, res: Response, next: NextFunction) 
         return;
     }
 
-        jwt.verify(token, process.env.JWT_SECRET as string, (err, decoded) => {
-            const payload = decoded as JwtPayload
-            if (err)
-              return res.status(403).json({ message: 'Invalid or expired token' });
-            (req as any).userId = payload.userId;
-            (req as any).role = payload.role
-            next()
-})
-    
+    jwt.verify(token, process.env.JWT_SECRET as string, (err, decoded) => {
+        const payload = decoded as JwtPayload;
+        if (err)
+            return res
+                .status(403)
+                .json({ message: "Invalid or expired token" });
+        (req as any).userId = payload.userId;
+        (req as any).role = payload.role;
+        next();
+    });
 };
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,11 +1,14 @@
 import express from "express";
-import { otpSchema, signInSchema, signUpSchema } from "../validation/userSchema";
+import {
+    otpSchema,
+    signInSchema,
+    signUpSchema,
+} from "../validation/userSchema";
 import { comparePassword, hashPassword } from "../utils";
 import jwt from "jsonwebtoken";
-import transporter from '../config/mailer';
+import transporter from "../config/mailer";
 import User from "../models/User";
 import Otp from "../models/Otp";
-
 
 const router = express.Router();
 const JWT_PASSWORD = process.env.JWT_PASSWORD || "password";
@@ -18,13 +21,13 @@ router.post("/signup", async (req, res) => {
         });
         return;
     }
-    const {name,email, password, phone,type } = validation.data
+    const { name, email, password, phone, type } = validation.data;
     const existingUser = await User.findOne({ email });
-    if (existingUser){
+    if (existingUser) {
         res.status(400).json({
-            message: "Email already exists"
-        })
-        return
+            message: "Email already exists",
+        });
+        return;
     }
 
     const hashedPassword = await hashPassword(password);
@@ -34,13 +37,18 @@ router.post("/signup", async (req, res) => {
         return;
     }
     try {
-        const newUser = new User({name, email, phone, password:hashedPassword})
-        newUser.save()
-        const payload = {userId: newUser.id, role: newUser.role}
-        const token = jwt.sign(payload, JWT_PASSWORD, { expiresIn: '1h' });
+        const newUser = new User({
+            name,
+            email,
+            phone,
+            password: hashedPassword,
+        });
+        newUser.save();
+        const payload = { userId: newUser.id, role: newUser.role };
+        const token = jwt.sign(payload, JWT_PASSWORD, { expiresIn: "1h" });
 
-        res.status(200).json({token});
-    } catch (e:any) {
+        res.status(200).json({ token });
+    } catch (e: any) {
         res.status(400).json({
             message: e.message,
         });
@@ -55,9 +63,9 @@ router.post("/login", async (req, res) => {
         });
         return;
     }
-    const {email, password} = validation.data
+    const { email, password } = validation.data;
     try {
-        const existingUser = await User.findOne({email})
+        const existingUser = await User.findOne({ email });
         if (!existingUser) {
             res.status(404).json({
                 message: "User not found",
@@ -71,8 +79,8 @@ router.post("/login", async (req, res) => {
             });
             return;
         }
-        const payload = {userId:existingUser.id, role: existingUser.role}
-        const token = jwt.sign(payload, JWT_PASSWORD, {expiresIn: '1h'});
+        const payload = { userId: existingUser.id, role: existingUser.role };
+        const token = jwt.sign(payload, JWT_PASSWORD, { expiresIn: "1h" });
         res.status(200).json({ token });
     } catch (e) {
         res.status(400).json({
@@ -81,69 +89,71 @@ router.post("/login", async (req, res) => {
     }
 });
 
-
-
 router.post("/otp/send", async (req, res) => {
-  try {
-    const vaildation = otpSchema.safeParse(req.body)
-    if(!vaildation.success){
-        res.status(400).json({
-            message: "Invalid email"
-        })
-        return
+    try {
+        const vaildation = otpSchema.safeParse(req.body);
+        if (!vaildation.success) {
+            res.status(400).json({
+                message: "Invalid email",
+            });
+            return;
+        }
+        const { email } = vaildation.data;
+        const otp = Math.floor(100000 + Math.random() * 900000).toString();
+        const expiresAt = new Date(Date.now() + 10 * 60 * 1000);
+        await Otp.findOneAndUpdate(
+            { email },
+            { otp, expiresAt },
+            { upsert: true, new: true }
+        );
+
+        const mailOptions = {
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: "Your OTP Code",
+            text: `Your OTP code is: ${otp}. It will expire in 10 minutes.`,
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error("Error sending OTP email:", error);
+                return res
+                    .status(500)
+                    .json({ message: "Error sending OTP email" });
+            }
+            res.status(200).json({ message: "OTP sent successfully" });
+        });
+    } catch (err) {
+        res.status(500).json({ message: (err as Error).message });
     }
-    const {email} = vaildation.data
-    const otp = Math.floor(100000 + Math.random() * 900000).toString();
-    const expiresAt = new Date(Date.now() + 10 * 60 * 1000); 
-    await Otp.findOneAndUpdate(
-        {email}, 
-        {otp, expiresAt},
-        {upsert: true, new: true}
-    )
-
-    const mailOptions = {
-      from: process.env.EMAIL_USER,
-      to: email,
-      subject: 'Your OTP Code',
-      text: `Your OTP code is: ${otp}. It will expire in 10 minutes.`,
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error('Error sending OTP email:', error);
-        return res.status(500).json({ message: 'Error sending OTP email' });
-      }
-      res.json({ message: 'OTP sent successfully' });
-    });
-  } catch (err) {
-    res.status(500).json({ message: (err as Error).message });
-  }
 });
 
 router.post("/otp/verify", async (req, res) => {
-  try {
-    const { email, otp } = req.body;
-    const otpRecord = await Otp.findOne(otp)
-    if (!otpRecord){
-        res.status(400).json({ message: 'No OTP requested for this email' });
-        return
-    }
+    try {
+        const { email, otp } = req.body;
+        const otpRecord = await Otp.findOne(otp);
+        if (!otpRecord) {
+            res.status(400).json({
+                message: "No OTP requested for this email",
+            });
+            return;
+        }
 
-    if (otpRecord.otp !== otp){
-        res.status(400).json({ message: 'Invalid OTP' });
-        return
-    }
+        if (otpRecord.otp !== otp) {
+            res.status(400).json({ message: "Invalid OTP" });
+            return;
+        }
 
-    if (otpRecord.expiresAt < new Date()){
-        res.status(400).json({ message: 'OTP expired' });
-        return
-    }
+        if (otpRecord.expiresAt < new Date()) {
+            res.status(400).json({ message: "OTP expired" });
+            return;
+        }
 
-    await Otp.deleteOne({email});
-    res.json({ message: 'OTP verified successfully' });
-  } catch (err) {
-    res.status(500).json({ message: (err as Error).message });
-  }
+        await Otp.deleteOne({ email });
+        res.status(200).json({ message: "OTP verified successfully" });
+    } catch (err) {
+        res.status(500).json({ message: (err as Error).message });
+    }
 });
 
 export default router;
