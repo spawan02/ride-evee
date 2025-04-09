8bb3eb232a961a4a1a4a706e9e17bcc216387b9b
pawan <spavandsp@gmail.com>
2025-04-10 01:12:19 +0530

    feat: added swagger

---
+++ src/swagger/swagger.ts
@@ -0,0 +1,30 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+import YAML from 'yamljs';
+import { Express } from 'express';
+import swaggerUi from 'swagger-ui-express';
+
+const swaggerDocument = YAML.load('./src/swagger/swagger.yaml');  
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'User API',
+      version: '1.0.0',
+      description: 'API documentation for managing gadgets in the inventory.',
+    },
+    servers: [
+      {
+        url: 'https://imf.itsarc.me/',
+        description: 'Production server on Railway',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.ts'], 
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
+export const setupSwaggerDocs = (app: Express) => {
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));  
+};
