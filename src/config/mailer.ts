b7860c919da62eb8cf55331d8623edf0af535a38
pawan <spavandsp@gmail.com>
2025-04-11 01:56:35 +0530

    fix: nodemailer

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -1,11 +1,16 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
+import dotenv from "dotenv";
+dotenv.config();
 const transporter = nodemailer.createTransport({
-  service: 'Gmail', 
-  auth: {
-    user: process.env.EMAIL_USER, 
-    pass: process.env.EMAIL_PASS, 
-  },
+    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
 });
 
 export default transporter;
